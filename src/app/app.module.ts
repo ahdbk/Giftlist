fa76245e1203765c16d52567ffceae8216edbace
Ahd bk <ahd.benkheder@gmail.com>
2023-10-07 19:21:37 +0200

    update home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { NgOptimizedImage } from '@angular/common';
+import { FooterComponent } from '../components/footer/footer.component';
+import { HeaderComponent } from '../components/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ButtonModule,
+    RippleModule,
+    NgOptimizedImage,
+    FooterComponent,
+    HeaderComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/modules/website/website.module.ts
+++ src/modules/website/website.module.ts
@@ -4,6 +4,8 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { WebsiteRoutingModule } from './website-routing.module';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 import { AboutSectionComponent } from './components/about-section/about-section.component';
+import { ButtonModule } from 'primeng/button';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @NgModule({
   declarations: [
@@ -11,6 +13,6 @@ import { AboutSectionComponent } from './components/about-section/about-section.
     HeroSectionComponent,
     AboutSectionComponent
   ],
-  imports: [CommonModule, WebsiteRoutingModule]
+  imports: [CommonModule, WebsiteRoutingModule, ButtonModule, HeaderComponent]
 })
 export class WebsiteModule {}
